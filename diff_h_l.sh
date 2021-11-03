0477e38ee5fef553d98f34d844ed181af6092f05
Philippe Turner <philippe.turner@polymtl.ca>
2021-11-03 15:03:18 -0400

    Update on diff script

--- diff_h_l.sh
+++ diff_h_l.sh
@@ -1,4 +1,5 @@
 for l in $(ls ./dataset)
 do
-    time hadoop jar ~/hadoop-3.3.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input/wordcount/dataset/$l output/$l
+    mytime="$(hadoop jar ~/hadoop-3.3.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input/wordcount/dataset/$l output/$l  ) 2>&1 1>/dev/null )"
+    echo $l $mytime
 done
