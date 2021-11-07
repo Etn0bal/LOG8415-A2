b15f679f115b60f0a267b513a3118bf74d5d3ec9
Etn0bal <antoinelabo@videotron.ca>
2021-11-07 19:37:52 +0000

    update all_word_count

--- WordCount/Spark/all_word_count.sh
+++ WordCount/Spark/all_word_count.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Start" > time.txt
 for filename in ../../dataset/*.txt; do 
-    time spark-submit ./word_count.py ../dataset/$filename ./${filename%.txt}/
+    basename $filename .txt >> time.txt;
+    { time spark-submit word_count.py $filename ./1/$(basename $filename .txt)/ ; } 2>> time.txt
+    { time spark-submit word_count.py $filename ./2/$(basename $filename .txt)/ ; } 2>> time.txt
+    { time spark-submit word_count.py $filename ./3/$(basename $filename .txt)/ ; } 2>> time.txt
 done
