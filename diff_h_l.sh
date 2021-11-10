3deaf75408c84d499b8f0acab46b49d9be13c08c
Philippe Turner <philippeturner@gmail.com>
2021-11-10 11:19:38 -0500

    Remove useless file

--- diff_h_l.sh
+++
@@ -1,5 +0,0 @@
-for l in $(ls ./dataset)
-do
-    mytime="$(hadoop jar ~/hadoop-3.3.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input/wordcount/dataset/$l output/$l  ) 2>&1 1>/dev/null )"
-    echo $l $mytime
-done
