f4a2955f64a01914ca486a779cf2d464120b7d64
Philippe Turner <philippeturner@gmail.com>
2021-11-10 11:35:06 -0500

    Fix parser

--- utils/results_parser.py
+++ utils/results_parser.py
@@ -8,6 +8,6 @@ with open(dir_name + "/" + file_name, "r") as f:
     regex = re.compile(r"(?<=user\t)(.*?)(?=\n)")
     ungroup = regex.findall(f.read())
     
-with open(dir_name + "/" + "results.csv", "w") as f:
+with open(dir_name + "/" + "results.csv", "a") as f:
     for res in ungroup:
-        f.write(f"{file_name},{res}\n")
+        f.write(f"{file_name},{res}")
